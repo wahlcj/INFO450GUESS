56d3df0af29a65c0a257a46255b965f73e73b386
Carter Wahl <CarterWahl@carters-mbp-2.fios-router.home>
2018-02-25 19:02:44 -0500

    Initial Commit

---
+++ wahlcj_GuessAssignment2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  wahlcj_GuessAssignment2
+//
+//  Created by Carter Wahl on 2/25/18.
+//  Copyright © 2018 Carter Wahl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
